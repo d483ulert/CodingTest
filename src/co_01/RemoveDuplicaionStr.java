26db95432cf069b3dce60e6e90bde1fdbc1f7a50
jisung324 <jisung3245@gmail.com>
2023-01-04 00:26:47 +0900

    중복문자제거

--- src/co_01/RemoveDuplicaionStr.java
+++ src/co_01/RemoveDuplicaionStr.java
@@ -5,9 +5,19 @@ import java.util.Scanner;
 public class RemoveDuplicaionStr {
 
     public static void main(String[] args) {
+        String answer="";
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
+        for(int i=0; i<str.length(); i++){
+            if(str.indexOf(str.charAt(i))==i){
+//                indexOf() 는 특정 문자나 문자열이 앞에서부터 처음 발견되는 인덱스를 반환하며
 
-
+                System.out.println("i: "+i);
+                System.out.println("str.charAt(): "+str.charAt(i));
+                System.out.println("str.indexOf(str.charAt(i)): "+str.indexOf(str.charAt(i)));
+                answer+=str.charAt(i);
+            }
+        }
+        System.out.println("answer: "+answer);
     }
 }

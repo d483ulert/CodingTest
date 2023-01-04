d2154a2db7605e9d9d2c07885f7594b9a92bb224
d483ulert <tottenham3@naver.com>
2023-01-04 22:38:35 +0900

    숫자만추출

---
+++ src/co_01/OnlyNan.java
@@ -0,0 +1,21 @@
+package co_01;
+
+import java.util.Scanner;
+
+public class OnlyNan {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String answer ="";
+
+        for(char x : str.toCharArray()){
+            if(x>=48 && x<= 57){
+                answer+=x;
+            }
+        }
+
+        System.out.println("answer: "+answer);
+    }
+}

690063c5cf25e57ad1cbe9d5caf204390b6dd9b3
jisung7153 <jisung.kim@mobin-inc.com>
2023-01-29 21:42:14 +0900

    후위식 연산

---
+++ src/co_02/PostFix.java
@@ -0,0 +1,35 @@
+package co_02;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PostFix {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int answer = 0;
+
+        Stack <Integer> stack = new Stack<>();
+        for(char x: str.toCharArray()) {
+            if(Character.isDigit(x)) { //isDigit 숫자 판별
+                stack.push(x-48);   // 아스키코드로 문자-48 해줘야 숫자
+            }else {
+                int rt=stack.pop();
+                int lt=stack.pop();
+                if(x=='+') {
+                    stack.push(lt+rt);
+                }else if(x=='-') {
+                    stack.push(lt-rt);
+                }else if(x=='*') {
+                    stack.push(lt*rt);
+                }else if(x=='/') {
+                    stack.push(lt/rt);
+                }
+            }
+        }
+        answer=stack.get(0);
+
+        System.out.println(answer);
+    }
+}

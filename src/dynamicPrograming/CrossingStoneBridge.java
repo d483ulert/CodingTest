feb0d0e90844d92938edc589d556cfe8c4e5ec84
jisung7153 <jisung.kim@mobin-inc.com>
2023-01-31 22:28:45 +0900

    돌계단 건너기

--- src/dynamicPrograming/CrossingStoneBridge.java
+++ src/dynamicPrograming/CrossingStoneBridge.java
@@ -1,9 +1,21 @@
 package dynamicPrograming;
 
-public class CrossingStoneBridge {
-    static int dy;
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class CrossingStoneBridge {
+    static int[]dy;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dy = new int[n+1];
+
+        dy[1]=1;
+        dy[2]=2;
+
+        for(int i=3; i<=n; i++){
+            dy[i]=dy[i-2]+dy[i-1];
+        }
+        System.out.println(dy[n]+1);
 
     }
 }

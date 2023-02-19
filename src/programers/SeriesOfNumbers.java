c50e342509bd28105597e8a4ee071baa3c73362b
jisung7153 <tottenham3@naver.com>
2023-02-19 21:47:30 +0900

    '연속된합의수'

---
+++ src/programers/SeriesOfNumbers.java
@@ -0,0 +1,18 @@
+package programers;
+
+public class SeriesOfNumbers {
+    class Solution {
+        public int[] solution(int num, int total) {
+            int[] answer = new int [num];
+
+            int nSum = num *(1+num)/2;
+            int start = (total - nSum)/ num;
+
+            for(int i=1; i<num+1; i++){
+                answer[i-1]=i+start;
+            }
+
+            return answer;
+        }
+    }
+}

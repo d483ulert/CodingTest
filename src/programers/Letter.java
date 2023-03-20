2644e515001f19027487f7914eee88f50d001b51
d483ulert <tottenham3@naver.com>
2023-03-20 21:23:00 +0900

    letter

---
+++ src/programers/Letter.java
@@ -0,0 +1,13 @@
+package programers;
+
+public class Letter {
+
+    public static void main(String[] args) {
+        String msg = "happy birthday!";
+        int answer= 0;
+
+        System.out.println(msg.length()*2);
+
+
+    }
+}

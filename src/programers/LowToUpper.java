15d50c7e9d853b52c4171732254701c375f3783b
jisung7153 <jisung.kim@mobin-inc.com>
2023-04-01 21:19:35 +0900

    대소문자변경
    Signed-off-by: jisung7153 <jisung.kim@mobin-inc.com>

---
+++ src/programers/LowToUpper.java
@@ -0,0 +1,26 @@
+package programers;
+
+public class LowToUpper {
+
+    public static void main(String[] args) {
+        String my_string = "cccCCC";
+        char [] c = new char[my_string.length()];
+
+        for(int i=0; i < my_string.length(); i++){
+            c[i]=my_string.charAt(i);
+        }
+
+        for(int i=0; i<my_string.length();i++){
+            if(Character.isUpperCase(c[i])){
+                c[i]= Character.toLowerCase(c[i]);
+            }
+
+            if(Character.isLowerCase(c[i])){
+                c[i]=Character.toUpperCase(c[i]);
+            }
+        }
+
+        String answer= String.valueOf(c);
+
+    }
+}

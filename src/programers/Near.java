d1efb91fb5c562b066bcbd4946a5998b89f27857
d483ulert <tottenham3@naver.com>
2023-04-08 22:52:40 +0900

    중복 문자 제거
    Signed-off-by: jisung7153 <jisung.kim@mobin-inc.com>

--- src/programers/Near.java
+++ src/programers/Near.java
@@ -1,17 +1,16 @@
 package programers;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Near {
 
     public static void main (String[] args) {
-        int [] sides = {1,2,3};
-        int answer= 2;
-        //가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야한다.
-        Arrays.sort(sides);
-        if(sides[0]+sides[1] > sides[2]){
-            answer = 1;
-        }
+        String my_string = "We are the world";
+
+        Arrays.asList(my_string.split("")).stream()
+                .distinct()
+                .collect(Collectors.joining());
 
     }
 }

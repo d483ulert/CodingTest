396b7cfd7805a59f64e51ea43a6832c521ab0a42
d483ulert <tottenham3@naver.com>
2023-05-31 21:31:26 +0900

    햄버거 만드기
    Signed-off-by: jisung7153 <jisung.kim@mobin-inc.com>

---
+++ src/programers/HamBurger.java
@@ -0,0 +1,26 @@
+package programers;
+
+public class HamBurger {
+
+    public static void main(String[] args) {
+        int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+        int answer =0;
+        //빵 야채 고기 빵 순서로
+        // 1: 빵 2: 야채 3: 고기
+
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                    && stack[sp - 2] == 3
+                    && stack[sp - 3] == 2
+                    && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
+            }
+        }
+
+
+    }
+}
